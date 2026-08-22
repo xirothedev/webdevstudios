f892e98372959d04e5e09fc2b73aa4d50d617eff
Xiro The Dev <lethanhtrung.trungle@gmail.com>
2026-08-22 22:04:28 +0700

    chore(api): remove unused SkipTransform escape hatch

--- apps/api/src/common/interceptors/transform.interceptor.ts
+++ apps/api/src/common/interceptors/transform.interceptor.ts
@@ -21,11 +21,8 @@
  */
 
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-import { SKIP_TRANSFORM_KEY } from '../decorators';
 
 export interface Response<T> {
   success: boolean;
@@ -37,21 +34,9 @@ export interface Response<T> {
 
 @Injectable()
 export class TransformInterceptor<T> implements NestInterceptor<T, Response<T> | T> {
-  constructor(private readonly reflector: Reflector) {}
-
   intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T> | T> {
     const request = context.switchToHttp().getRequest();
     const path = request.url;
-
-    // Check if transform should be skipped
-    const skipTransform = this.reflector.getAllAndOverride<boolean>(SKIP_TRANSFORM_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (skipTransform) {
-      return next.handle();
-    }
 
     return next.handle().pipe(
       map((data) => ({
